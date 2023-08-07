accc2497b6478ecea3db218fbdccf6d4991b0b07
Muhammad Ghifari <ghifari9567@gmail.com>
2023-08-07 20:21:55 +0700

    Initial commit

---
+++ Ubersnap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ubersnap
+//
+//  Created by Muhammad Ghifari on 7/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
